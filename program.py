c480c0f2dbe275a91f185a05387794b8b59d6b63
Esteak Shapin <esteakshapin@gmail.com>
2022-09-07 23:17:29 -0400

    completed assignment 5

---
+++ program.py
@@ -0,0 +1,9 @@
+#Name: Esteak Shapin #Email: esteak.shapin50@myhunter.cuny.edu
+#Date: September 7, 2022
+#This program repeats the phrase "Practice makes perfect." as the inputed integer
+
+print("Enter repetition time:", end =" ")
+x = int(input())
+
+for i in range(x):
+    print("Practice makes perfect.")
