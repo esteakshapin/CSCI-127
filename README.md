9c44d2148746a7e7dd93a6f9b20c537cb7f5628a
Esteak Shapin <esteakshapin@gmail.com>
2022-09-07 22:54:01 -0400

    completed assignment 3

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # CSCI-127
+
+## heading
+
+```
+#Name: Esteak Shapin #Email: esteak.shapin50@myhunter.cuny.edu
+#Date: September 7, 2022
+#This program {program descript}
+```
---
+++ program3.py
@@ -0,0 +1,18 @@
+#Name: Esteak Shapin #Email: esteak.shapin50@myhunter.cuny.edu
+#Date: September 7, 2022
+#This program prints a flower
+
+import turtle
+
+t = turtle.Turtle()
+
+for i in range(9):
+    t.forward(100)
+    t.left(105)
+    t.forward(52)
+    t.left(105)
+    t.forward(100)
+    t.right(170)
+
+# keeping window open
+turtle.done()
