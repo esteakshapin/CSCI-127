78e83bd53bb0ec3426f1e33a1a30e41b057c2fa6
Esteak Shapin <esteakshapin@gmail.com>
2022-10-14 21:18:51 -0400

    completed assignment 21

--- program.py
+++ program.py
@@ -6,34 +6,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+fileName = input("Enter file name: ")
+img = plt.imread(fileName)
+countSnow = 0
+threshold = float(input("Enter threshold: "))
 
-#Read in the data to an array, called elevations:
-elevations = np.loadtxt('elevationsNYC.txt')
+for row in range(img.shape[0]):
+    for col in range(img.shape[1]):
+        #Check if red, green, and blue are > t:
+          if (img[row,col,0] > threshold) and (img[row,col,1] > threshold) and (img[row,col,2] > threshold):
+               countSnow = countSnow + 1
 
-#Take the shape (dimensions) of the elevations
-#  and add another dimension to hold the 3 color channels:
-mapShape = elevations.shape + (3,)
-
-#Create a blank image that's all zeros:
-floodMap = np.zeros(mapShape)
-
-for row in range(mapShape[0]):
-    for col in range(mapShape[1]):
-        if elevations[row,col] <= 0:
-            #Below sea level
-           floodMap[row,col,2] = 1.0     #Set the blue channel to 100%
-        elif elevations[row,col] <= 6:
-            #sets red to yellow
-           floodMap[row,col,0] = 1.0     #Set the red channel to 100%
-           floodMap[row,col,1] = 1.0
-        else:
-            #Above the 6 foot storm surge and didn't flood
-            floodMap[row,col,1] = 1.0   #Set the green channel to 100%
-        
-        if elevations[row,col] > 6 and elevations[row,col] <= 20:
-            floodMap[row,col,0] = .5 
-            floodMap[row,col,1] = .5 
-            floodMap[row,col,2] = .5 
-
-#Save the image:
-plt.imsave('floodMap.png', floodMap)
+print("Number of white pixels", countSnow)
+print("percent of white pixels: ", countSnow / (img.shape[0] * img.shape[1]) * 100)
